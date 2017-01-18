a128ebbdc7b4fe8b2d9d93b4b9dd02d81bd5b137
Jan Niehues <jan.niehues@kit.edu>
2017-01-18 13:28:48 +0100

    First version separate attention

---
+++ src/lamtram/separate-multitask-encoder-attentional.h
@@ -0,0 +1,82 @@
+#pragma once
+
+#include <lamtram/sentence.h>
+#include <lamtram/ll-stats.h>
+#include <lamtram/linear-encoder.h>
+#include <lamtram/encoder-attentional.h>
+#include <lamtram/multitask-model.h>
+#include <lamtram/neural-lm.h>
+#include <lamtram/extern-calculator.h>
+#include <lamtram/mapping.h>
+#include <dynet/dynet.h>
+#include <vector>
+#include <iostream>
+#include <lamtram/gru-cond.h>
+
+namespace dynet {
+class Model;
+struct ComputationGraph;
+struct Parameter;
+struct RNNBuilder;
+}
+
+namespace lamtram {
+
+
+// A class to calculate extern_calcal context
+class MultiTaskExternAttentional : public ExternAttentional {
+public:
+
+    MultiTaskExternAttentional(const std::vector<LinearEncoderPtr> & encoders,
+                      const std::string & attention_type, const std::string & attention_hist,
+                      int state_size, const std::string & lex_type,
+                      const std::vector<DictPtr> & vocab_src, const std::vector<DictPtr> & vocab_trg,
+                     int attention_context, bool source_word_embedding_in_softmax, int source_word_embedding_in_softmax_context, std::string encoder_type,
+                      dynet::Model & mod);
+    virtual ~MultiTaskExternAttentional() { }
+
+
+    // Reading/writing functions
+    static MultiTaskExternAttentional* Read(std::istream & in, const std::vector<DictPtr> & vocab_src, const std::vector<DictPtr> & vocab_trg, std::vector<MultiTaskModelPtr> & mtmodels, dynet::Model & model);
+    virtual void Write(std::ostream & out);
+
+
+
+protected:
+
+    const std::string encoder_type_;
+
+};
+
+typedef std::shared_ptr<MultiTaskExternAttentional> MultiTaskExternAttentionalPtr;
+
+
+
+// A class for feed-forward neural network LMs
+class SeparateMultiTaskEncoderAttentional : public EncoderAttentional {
+
+public:
+
+    // Create a new SeparateMultiTaskEncoderAttentional and add it to the existing model
+    SeparateMultiTaskEncoderAttentional(const ExternAttentionalPtr & extern_calc,
+                       const NeuralLMPtr & decoder, std::string decoder_type_,
+                       dynet::Model & model);
+    ~SeparateMultiTaskEncoderAttentional() { }
+
+    // Reading/writing functions
+    static SeparateMultiTaskEncoderAttentional* Read(const std::vector<DictPtr> & vocab_src, const std::vector<DictPtr> & vocab_trg, std::istream & in, std::vector<MultiTaskModelPtr> & mtmodels, dynet::Model & model);
+    virtual void Write(std::ostream & out);
+
+    static std::string ModelID() { return "multitaskencatt"; }
+
+
+protected:
+
+
+    const std::string decoder_type_;
+
+};
+
+typedef std::shared_ptr<SeparateMultiTaskEncoderAttentional> SeparateMultiTaskEncoderAttentionalPtr;
+
+}
---
+++ src/lamtram/shared-wordembedding-multitask-linear-encoder.h
@@ -0,0 +1,65 @@
+#pragma once
+
+#include <lamtram/sentence.h>
+#include <lamtram/linear-encoder.h>
+#include <lamtram/ll-stats.h>
+#include <lamtram/builder-factory.h>
+#include <lamtram/multitask-model.h>
+#include <dynet/dynet.h>
+#include <dynet/expr.h>
+#include <vector>
+#include <iostream>
+
+namespace dynet {
+class Model;
+struct ComputationGraph;
+struct LookupParameter;
+struct RNNBuilder;
+}
+
+namespace lamtram {
+
+// A class for feed-forward neural network LMs
+class SharedWordEmbeddingMultiTaskLinearEncoder : public LinearEncoder , public MultiTaskModel{
+    friend class EncoderAttentional;
+
+public:
+
+    // Create a new SharedWordEmbeddingMultiTaskLinearEncoder and add it to the existing model
+    SharedWordEmbeddingMultiTaskLinearEncoder(const std::vector<int> & vocab_size, int wordrep_size,
+             const BuilderSpec & hidden_spec, int unk_id,
+             dynet::Model & model);
+    ~SharedWordEmbeddingMultiTaskLinearEncoder() { }
+
+
+    // Reading/writing functions
+    static SharedWordEmbeddingMultiTaskLinearEncoder* Read(std::istream & in, dynet::Model & model);
+    virtual void Write(std::ostream & out);
+
+    static std::string ModelID() { return "separatemultitasklinearencoder"; }
+
+    // Accessors
+    int GetVocabSize() const { std::cerr << "Get VocabSize not supported for multi-task" << std::endl; exit(-1); }
+    
+    void SetVocabulary(int sourceIndex, int targetIndex) {current_voc_ = sourceIndex; p_wr_W_ = ps_wr_W_[current_voc_];builder_ = builders_[current_voc_];};
+
+    virtual void SetDropout(float dropout);
+
+protected:
+
+    std::vector<int> vocab_sizes_;
+
+    // Pointers to the parameters
+    std::vector<dynet::LookupParameter> ps_wr_W_; // Wordrep weights
+
+    int current_voc_;
+
+    // The RNN builder
+    std::vector<BuilderPtr> builders_;
+
+
+};
+
+typedef std::shared_ptr<SharedWordEmbeddingMultiTaskLinearEncoder> SharedWordEmbeddingMultiTaskLinearEncoderPtr;
+
+}
